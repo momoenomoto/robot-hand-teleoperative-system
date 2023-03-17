7ca8bafa510ab1951bccc5e53bde915140a4bf7f
Momoe Nomoto <mn2668@nyu.edu>
2023-03-17 18:04:06 +0800

    change main to use rtos

--- data_glove/main.c
+++ data_glove/main.c
@@ -1,62 +1,85 @@
-//#include "cmsis_os2.h"                  // ::CMSIS:RTOS2
+#include "cmsis_os2.h"                  // ::CMSIS:RTOS2
 #include "stm32f7xx.h"                  // Device header
 #include "Open746i_lcd.h"
 #include "Driver_USART.h"
-#include <stdio.h>
 
 #define UART_ITEM_COUNT 10
 #define BUFFER_SIZE 5
-#define IN_MIN 0.0
-#define OUT_MIN 300
-#define IN_MAX 5.0
-#define OUT_MAX 600
 
-void SysTick_Handler(void);
 void SystemClock_Config(void);
 extern ARM_DRIVER_USART Driver_USART1;
-void TIM5_IRQHandler(void);
 int map(double x, double in_min, double in_max, double out_min, double out_max);
+void TIM5_IRQHandler(void);
+void LCD_thread(void *argument) __NO_RETURN;
+void UART_thread(void *argument) __NO_RETURN;
+void TIM_thread(void *argument) __NO_RETURN;
+void ADC_thread(void *argument) __NO_RETURN;
+void UART_handler(uint32_t event);
+void ADC_IRQHandler(void);
 
-static unsigned short buffer[BUFFER_SIZE] = {0};
+static uint16_t buffer[BUFFER_SIZE] = {0};
 static double vout;
-static char string[50];
-static int i = 0;
 
+osEventFlagsId_t evt_id;
+osThreadId_t thrd_id1, thrd_id2, thrd_id3, thrd_id4;
 
-void TIM5_IRQHandler(void){ 
-	TIM5->SR = ~(1 << 0);
-	ADC1->CR2 |= (1 << 30);
-	while(!(ADC1->SR & (1<<1))); // wait while conversion not complete
-	vout = (ADC1->DR * 5.0) / 4096;
-	if (vout > 4095)
-		vout = 4095;
-	sprintf(string, "%.3fV", vout);
-	BSP_LCD_DisplayStringAt(0, 300, string, CENTER_MODE);
-	BSP_LCD_DisplayStringAt(i, map(vout, IN_MIN, IN_MAX, OUT_MIN, OUT_MAX), 'o', LEFT_MODE);
-	i++;
-	//buffer[0] = vout; 
-	//sprintf(string, "%04u", buffer[0]);
-	//BSP_LCD_DisplayStringAt(0, 300, string, CENTER_MODE);
-	//while (Driver_USART1.GetStatus().tx_busy == 1);
-	//Driver_USART1.Send(buffer, UART_ITEM_COUNT);
+// const osThreadAttr_t thrd1_attr = {
+//   .priority  = osPriorityHigh,
+// };
+// const osThreadAttr_t thrd2_attr = {
+//   .priority  = osPriorityLow,
+// };
+
+void ADC_IRQHandler(void)
+{
+	if (ADC1->SR & ADC_SR_EOC)
+	{
+		BSP_LCD_DisplayStringAt(0, 400, "here", CENTER_MODE); // this is displayed on LCD
+		osEventFlagsSet(evt_id, 8);
+	}
 }
 
-
-int main(void)
+__NO_RETURN void ADC_thread(void *argument)
 {
-	SystemClock_Config();
-
 	RCC->AHB1ENR |= (1 << 0); // Activate GPIOA
-	RCC->APB1ENR |= (1 << 3); //Activate timer 5
 	RCC->APB2ENR |= (1 << 8); // Activate ADC1
 	GPIOA->MODER |= (3 << 2);  // PA1 for analog mode
 
 	// ADC1_IN1
 	ADC1->SQR3 |= (1 << 0);
-	ADC1->CR2 |= (1 << 0); // activates output channel 1 
+	ADC1->CR2 |= (1 << 0); // activates output channel 1
+	ADC1->CR1 |= (1 << 5); // EOCIE bit set, generate interrupt
+
+	NVIC_EnableIRQ(ADC_IRQn);
+	
+	while (1)
+	{
+		osEventFlagsWait(evt_id, 8, osFlagsWaitAny, osWaitForever);
+		vout = (ADC1->DR * 3.3) / 4096;
+		if (vout > 4095)
+			vout = 4095;
+		buffer[0] = (uint16_t) (vout * 1000); 
+		osEventFlagsSet(evt_id, 3);
+	}
+	
+}
+
+
+void TIM5_IRQHandler(void){ 
+	TIM5->SR = ~(1 << 0);
+	ADC1->CR2 |= (1 << 30);
+	
+	//while(!(ADC1->SR & (1<<1))); // wait while conversion not complete
+			
+}
+
+__NO_RETURN void TIM_thread(void *argument)
+{
+	RCC->AHB1ENR |= (1 << 0); // Activate GPIOA
+	RCC->APB1ENR |= (1 << 3); //Activate timer 5
 
 	// 180Hz timer
-	TIM5->PSC=179;
+	TIM5->PSC = 899;
 	TIM5->ARR = 59999; // 1 Hz
 	//TIM5->PSC = 9;
 	//TIM5->ARR = 59999;
@@ -65,14 +88,26 @@ int main(void)
 	TIM5->DIER |= (1 << 0); // enable update interrupt
 
 	NVIC_EnableIRQ(TIM5_IRQn);
-	
-	BSP_LCD_Init();
-	BSP_LCD_Clear(LCD_COLOR_BLUE);
-	BSP_LCD_DisplayStringAt(0, 0, (unsigned char *) "Test LCD", CENTER_MODE);
 
+	while(1)
+	{
+		osThreadYield();
+	}
+}
+
+void UART_handler(uint32_t event)
+{
+	if (event & ARM_USART_EVENT_SEND_COMPLETE)
+	{
+		osEventFlagsSet(evt_id, 4);
+	}
+}
+
+
+__NO_RETURN void UART_thread(void *argument)
+{
 	// initialize USART
-	/*
-	Driver_USART1.Initialize(NULL);
+	Driver_USART1.Initialize(UART_handler);
 	Driver_USART1.PowerControl(ARM_POWER_FULL);
 	Driver_USART1.Control(ARM_USART_MODE_ASYNCHRONOUS | 
 												ARM_USART_DATA_BITS_8 | 
@@ -80,21 +115,29 @@ int main(void)
 												ARM_USART_STOP_BITS_1 |
 												ARM_USART_FLOW_CONTROL_NONE, 9600);
 	Driver_USART1.Control(ARM_USART_CONTROL_TX, 1);
-	Driver_USART1.Control(ARM_USART_CONTROL_RX, 1);*/
+	Driver_USART1.Control(ARM_USART_CONTROL_RX, 1);
 	
-	// osKernelInitialize();
-	// evt_id = osEventFlagsNew(NULL);
-	// q_id = osMessageQueueNew(MSGQUEUE_OBJECTS, sizeof(MSGQUEUE_OBJ_t), NULL);
-	// thrd_id1 = osThreadNew(UART_thread, NULL, &thrd1_attr);
-	// thrd_id2 = osThreadNew(LCD_thread, NULL, &thrd2_attr);
-	// thrd_id3 = osThreadNew(SERVO_thread, NULL, NULL);
-	
-	// osKernelStart();
+	while(1)
+	{
+		Driver_USART1.Send(buffer, UART_ITEM_COUNT);
+		osEventFlagsWait(evt_id, 5, osFlagsWaitAll, osWaitForever);
+		
+	}
+}
 
+__NO_RETURN void LCD_thread(void *argument)
+{
+	char string[50];
+	char string2[50];
 
-	while(1);
-
-	return 0;
+	while(1)
+	{
+		osEventFlagsWait(evt_id, 2, osFlagsWaitAny, osWaitForever);
+		sprintf(string, "%.3fV", vout);
+		BSP_LCD_DisplayStringAt(0, 300, string, CENTER_MODE);
+		sprintf(string2, "0x%04x%04x%04x%04x%04x", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4]);
+		BSP_LCD_DisplayStringAt(0, 500, string2, CENTER_MODE);
+	}
 }
 
 int map(double x, double in_min, double in_max, double out_min, double out_max)
@@ -102,11 +145,32 @@ int map(double x, double in_min, double in_max, double out_min, double out_max)
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-
-void SysTick_Handler(void)
+int main(void)
 {
-	HAL_IncTick();
+	SystemClock_Config();
+
+	BSP_LCD_Init();
+	BSP_LCD_Clear(LCD_COLOR_BLUE);
+	BSP_LCD_DisplayStringAt(0, 0, (unsigned char *) "Test LCD", CENTER_MODE);
+	
+	osKernelInitialize();
+	evt_id = osEventFlagsNew(NULL);
+	thrd_id1 = osThreadNew(UART_thread, NULL, NULL);
+	thrd_id2 = osThreadNew(LCD_thread, NULL, NULL);
+	thrd_id3 = osThreadNew(ADC_thread, NULL, NULL);
+	thrd_id4 = osThreadNew(TIM_thread, NULL, NULL);
+	
+	osKernelStart();
+
+	while(1); // reached only if there's an error
+
+	return 0;
 }
+
+// void SysTick_Handler(void)
+// {
+// 	HAL_IncTick();
+// }
 
 void SystemClock_Config(void)
 {
